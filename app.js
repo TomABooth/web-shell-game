503b045af02680df1bab0d1548e553d6636915ae
Tom Booth <tombeezle@gmail.com>
2022-09-12 16:17:06 -0700

    added lots of java, and wireframe, forgot to commit earlier

--- app.js
+++ app.js
@@ -1,15 +1,64 @@
 /* Imports */
-// import { getRandomItem } from './utils.js';
+import { getRandomItem } from './utils.js';
 
 /* State */
+let gameState = 'guess'; // 'guess' or 'results'
+let guess = ''; // 'left', 'center' or 'right'
+let pearl = ''; // 'left', 'center' or 'right'
+let result = ''; // 'win' or 'lose'
+
+let total = 0;
+let wins = 0;
+
+// probability array
+const shell = ['left', 'center', 'right'];
 
 /* Actions */
-function loadPage() {}
+function loadPage() {
+    displayGuess();
+    displayResults();
+    displayScoreboard();
+}
+
+function liftShell(userGuess) {
+    gameState = 'results';
+    guess = userGuess;
+    pearl = getRandomItem(shell);
+    total++;
+
+    if (guess === pearl) {
+        result = 'win';
+        wins++;
+    } else {
+        result = 'lose';
+    }
+
+    loadPage();
+}
+
+function playAgain() {
+    gameState = 'guess';
+    loadPage();
+}
 
 /* Components */
 
-/* Component */
+/* Scoreboard */
+const totalDisplay = document.getElementById('total-display');
+const winsDisplay = document.getElementById('wins-display');
+const lossesDisplay = document.getElementById('losses-display');
+
+function displayScoreboard() {
+    totalDisplay.textContent = total;
+    winsDisplay.textContent = wins;
+    lossesDisplay.textContent = total - wins;
+}
+
+/* Guess */
 // get DOM
+const guessLeft = document.getElementById('guess-left');
+const guessCenter = document.getElementById('guess-center');
+const guessRight = document.getElementById('guess-right');
 // display
 // event listeners
 
