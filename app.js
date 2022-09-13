6c76dd53de94ed357b3841639e6bbd91195a8bb4
Tom Booth <tombeezle@gmail.com>
2022-09-12 17:48:04 -0700

    added presentation logit to display guess state

--- app.js
+++ app.js
@@ -6,15 +6,29 @@ let gameState = 'guess'; // 'guess' or 'results'
 
 /* Actions */
 function loadPage() {
-    displayShells();
+    displayShells(gameState);
 }
-function displayShells() {}
+function displayShells(gameState) {
+    if (gameState === 'guess') {
+        results.classList.add('hidden');
+        playAgainButton.classList.add('hidden');
+    }
+}
 
 /* Components */
 
 /* Component */
+
 // get DOM
+const results = document.getElementById('results');
+const playAgainButton = document.getElementById('play-again-button');
+
 // display
+function displayGuess() {
+    results.classList.add('hidden');
+    playAgainButton.classList.add('hidden');
+}
+
 // event listeners
 
 /* Run page load code */
