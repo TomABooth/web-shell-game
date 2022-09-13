0a8376c63e7e4571318336de283f8cd2d897b793
Tom Booth <tombeezle@gmail.com>
2022-09-12 18:00:34 -0700

    added event listeners

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ function displayShells(gameState) {
 // get DOM
 const results = document.getElementById('results');
 const playAgainButton = document.getElementById('play-again-button');
+const guessLeft = document.getElementById('guess-1');
+const guessCenter = document.getElementById('guess-2');
+const guessRight = document.getElementById('guess-3');
 
 // display
 function displayGuess() {
@@ -30,6 +33,15 @@ function displayGuess() {
 }
 
 // event listeners
+guessLeft.addEventListener('click', () => {
+    liftShell('left');
+});
+guessCenter.addEventListener('click', () => {
+    liftShell('Center');
+});
+guessRight.addEventListener('click', () => {
+    liftShell('Right');
+});
 
 /* Run page load code */
 loadPage();
